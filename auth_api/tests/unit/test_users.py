5aa2a6e591661d12fa38736725b4f802bc0ca5b3
Liceth Yaneth <liyarozoquin12@gmail.com>
2023-07-14 16:35:25 -0500

    :white_check_mark: test user can be created (green)

--- auth_api/tests/unit/test_users.py
+++ auth_api/tests/unit/test_users.py
@@ -6,32 +6,17 @@ from rest_framework import status
 from django.utils import timezone
 from django.forms.models import model_to_dict
 from django.utils.dateparse import parse_datetime
-
+from auth_api.tests.unit.factories.user_factories import UserFactory
 class UserTestCase(TestCase):
 
     base_url='/users/api/v1/users'
     def setUp(self):
-        self.faker = Faker()
         self.client = APIClient()
 
         for i in range(5):
-            user_data={
-                'password': self.faker.password(length=10, special_chars=True),
-                'last_login': timezone.make_aware(self.faker.date_time_this_month(),timezone.get_current_timezone()),
-                'is_superuser': False,
-                'username': self.faker.user_name(),
-                'first_name': self.faker.first_name(),
-                'last_name': self.faker.last_name(),
-                'email': self.faker.email(),
-                'is_staff': False,
-                'is_active': True,
-                'date_joined': timezone.make_aware(self.faker.date_time_this_month(),timezone.get_current_timezone()),
-            }
-            
-            User.objects.create(**user_data)
+            User.objects.create(**UserFactory.build_user_JSON())
 
         self.expected_users=User.objects.all()
-
 
     def test_users_can_be_queried(self):
 
@@ -52,12 +37,25 @@ class UserTestCase(TestCase):
         expected_user_dict = model_to_dict(self.expected_users[0])
         response = self.client.get(f'{self.base_url}/{str(expected_user_dict["id"])}/')
         actual_user = response.json()
-        #Verificar que la respuesta tiene un código de estado HTTP 200 
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        
-
-        #Verificar que la información del usurio coincide
         actual_user['last_login'] = parse_datetime(actual_user['last_login'])
         actual_user['date_joined'] = parse_datetime(actual_user['date_joined'])
         
+
+        #Verificar que la respuesta tiene un código de estado HTTP 200 
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        
+        #Verificar que la información del usurio coincide
         self.assertEqual(actual_user, expected_user_dict)
+
+    def test_can_be_created(self):
+        user = UserFactory.build_user_JSON()
+        response= self.client.post(self.base_url+'/',user,format='json')
+        actual_user = response.json()
+        actual_user['last_login'] = parse_datetime(actual_user['last_login'])
+        actual_user['date_joined'] = parse_datetime(actual_user['date_joined'])
+
+        #Verificar que la respuesta tiene un código de estado HTTP 201
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        #Verificar que la información del usurio coincide con el creado
+        self.assertEqual(actual_user['username'], user['username'])
+
