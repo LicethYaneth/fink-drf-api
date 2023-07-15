842bce9f3fad3a96bfe5238aa00ebf413f30d47c
Liceth Yaneth <liyarozoquin12@gmail.com>
2023-07-15 15:42:27 -0500

    :white_check_mark: test can get seller by id (green)

--- api/serializers/seller_serializer.py
+++ api/serializers/seller_serializer.py
@@ -1,9 +1,10 @@
 from api.models.seller_model import Seller
 from rest_framework import serializers
-
+from api.serializers.branch_store_serializer import BranchStoreSerializer
+from auth_api.serializers.user_serializer import UserSerializer
 class SellerSerializer(serializers.ModelSerializer):
-    
     class Meta:
         model = Seller
         fields = '__all__'
         read_only_fields = ['created_at', 'updated_at']
+        
