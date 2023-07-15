2023bddef4810b2f09f2ad951af5b3cfedb71c40
Liceth Yaneth <liyarozoquin12@gmail.com>
2023-07-15 16:44:03 -0500

    :sparkles: Montly Balance endpoint

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path, include
 from api.views.branch_store_view import BranchStoreView
 from api.views.seller_view import SellerView
+from api.views.monthly_record_view import MonthlyRecordView
 from rest_framework import routers
 
 router = routers.DefaultRouter()
 router.register(r'branch_stores',BranchStoreView,basename='branch_stores')
 router.register(r'sellers',SellerView,basename='sellers')
-urlpatterns=[path("", include(router.urls))]
+urlpatterns=[
+    path("", include(router.urls)),
+    path('balance-monthly/', MonthlyRecordView.as_view(), name='balance-monthly'),
+]
