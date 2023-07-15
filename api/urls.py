92e73dc536c312f6bcd7ae39c93222464eabed28
Liceth Yaneth <liyarozoquin12@gmail.com>
2023-07-15 01:18:27 -0500

    :white_check_mark: upload

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
+from api.views.branch_store_view import BranchStoreView
 from rest_framework import routers
-from auth_api.views.user_view import UserView
 
 router = routers.DefaultRouter()
-router.register(r'users',UserView,basename='users')
+router.register(r'branch_stores',BranchStoreView,basename='branch_stores')
 urlpatterns=[path("", include(router.urls))]
--- auth_api/tests/unit/factories/user_factories.py
+++ auth_api/tests/unit/factories/user_factories.py
@@ -1,5 +1,4 @@
 from faker import Faker
-from django.contrib.auth.models import User
 from django.utils import timezone
 
 faker = Faker()
