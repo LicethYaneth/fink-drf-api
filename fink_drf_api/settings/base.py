b78f72944219fccad07208ecca92c694e996de19
Liceth Yaneth <liyarozoquin12@gmail.com>
2023-07-15 18:23:00 -0500

    :rocket: ready for deploy

--- fink_drf_api/settings/base.py
+++ fink_drf_api/settings/base.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -9,6 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "django-insecure-^vk33)cqj))z+)oz$1ebppvccipk$=tdnf(4o09^^qcl*!13av"
+SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 
 # Application definition
 
@@ -29,6 +31,7 @@ INSTALLED_APPS = BASE_APPS + LOCAL_APPS + THIRD_APPS
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -92,6 +95,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
