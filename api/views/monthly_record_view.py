856ac2895b0822267c03c3117316c01ea9f6a8ac
Liceth Yaneth <liyarozoquin12@gmail.com>
2023-07-15 17:04:50 -0500

    :sparkles: Sorted Numbers endpoint

--- api/views/monthly_record_view.py
+++ api/views/monthly_record_view.py
@@ -12,35 +12,43 @@ class MonthlyRecordView(APIView):
     permission_classes = [IsAuthenticated]
 
     def post(self,request):
-        authorization_header = request.META.get('HTTP_AUTHORIZATION')
-        auth_type, token = authorization_header.split(' ')
-        data = request.data
-        anual_balance = []
-        for i in range(len(data['Mes'])):
-            month = data['Mes'][i]
-            sales = data['Ventas'][i]
-            costs = data['Gastos'][i]
-            balance = sales - costs
-            user = UserUtil.get_user_info(token)
-            branch = UserUtil.get_branch(user)
-            MonthlyRecord.objects.create(
-                month=month,
-                sales=sales,
-                costs=costs,
-                balance=balance,
-                user=user,
-                branch=branch
+        try: 
+            authorization_header = request.META.get('HTTP_AUTHORIZATION')
+            auth_type, token = authorization_header.split(' ')
+            data = request.data
+            anual_balance = []
+            for i in range(len(data['Mes'])):
+                month = data['Mes'][i]
+                sales = data['Ventas'][i]
+                costs = data['Gastos'][i]
+                balance = sales - costs
+                user = UserUtil.get_user_info(token)
+                branch = UserUtil.get_branch(user)
+                MonthlyRecord.objects.create(
+                    month=month,
+                    sales=sales,
+                    costs=costs,
+                    balance=balance,
+                    user=user,
+                    branch=branch
+                )
+                monthly_balance = {
+                    'mes': month,
+                    'Ventas': sales,
+                    'Gastos': costs,
+                    'Balance': balance
+                }
+                anual_balance.append(monthly_balance)
+            return Response(
+                data={
+                    'anual_balance': anual_balance
+                },
+                status=status.HTTP_200_OK
             )
-            monthly_balance = {
-                'mes': month,
-                'Ventas': sales,
-                'Gastos': costs,
-                'Balance': balance
-            }
-            anual_balance.append(monthly_balance)
-        return Response(
-            data={
-                'anual_balance': anual_balance
-            },
-            status=status.HTTP_200_OK
-        )
+        except: 
+            return Response(
+                data={
+                    "message": "Error en el servicio"
+                },
+                status=status.HTTP_400_BAD_REQUEST
+            )
---
+++ api/views/sorted_number.py
@@ -0,0 +1,35 @@
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import status
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
+from api.models.monthly_record_model import MonthlyRecord
+from api.utils.user_util import UserUtil
+
+class SortedNumberView(APIView):
+    def post(self, request):
+      unsorted_numbers = request.data.get("sin_clasificar")
+      if unsorted_numbers is None or len(unsorted_numbers) <= 0:
+          return Response(
+              data={
+                  "Message": "La lista es nula"
+              },
+              status=status.HTTP_400_BAD_REQUEST
+          )
+      non_duplicates = []
+      duplicates = []
+
+      for element in unsorted_numbers:
+          if element not in non_duplicates:
+              non_duplicates.append(element)
+          else:
+              duplicates.append(element)
+
+      sorted_list = sorted(non_duplicates) + sorted(duplicates)
+      return Response(
+          data={
+              'sin clasificar': unsorted_numbers,
+              "clasificado": sorted_list
+          },
+          status=status.HTTP_200_OK
+      )
